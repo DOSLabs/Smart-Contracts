3052eb78311a06a0c965f1869a6b3c64e1a78a68
dienhv <dienhouit05@gmail.com>
2023-05-12 11:03:49 +0700

    update contract bridge

---
+++ deploy/MetaDOSAssetProxy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract MetaDOSAssetProxy is TransparentUpgradeableProxy {
+    constructor(
+        address logic_,
+        address admin_
+    ) TransparentUpgradeableProxy(logic_, admin_, "") {}
+}
