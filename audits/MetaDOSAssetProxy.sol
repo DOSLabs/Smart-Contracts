21775b9a94203d154df0f8373d02ee949847ef7f
dienhv <dienhouit05@gmail.com>
2023-09-15 10:33:02 +0700

    ht

--- audits/MetaDOSAssetProxy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-
-contract MetaDOSAssetProxy is TransparentUpgradeableProxy {
-    constructor(address logic_, address admin_) TransparentUpgradeableProxy(logic_, admin_, "") {}
-}
