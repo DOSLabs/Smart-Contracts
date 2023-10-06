8573ea3500639b5b9bc9e5486836bf8b60e1eec6
dienhv <dienhouit05@gmail.com>
2023-10-07 00:58:32 +0700

    update audit

--- audits/MetaDOSAssetOriginal.sol
+++ audits/MetaDOSAssetOriginal.sol
@@ -4,8 +4,26 @@ pragma solidity ^0.8.0;
 import "./OERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155ReceiverUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
 
-contract MetaDOSAssetOriginal is OERC1155Upgradeable, IERC1155ReceiverUpgradeable {
+contract MetaDOSAssetOriginal is
+    EIP712Upgradeable,
+    OERC1155Upgradeable,
+    IERC1155ReceiverUpgradeable
+{
+    using ECDSAUpgradeable for bytes32;
+
+    struct MintRequest {
+        address to;
+        uint256 id;
+        uint256 nonce;
+        uint256 amount;
+    }
+    bytes32 private constant _MINTHASH =
+        keccak256(
+            "MintRequest(address to,uint256 id,uint256 nonce,uint256 amount)"
+        );
+
     // Token name
     string private _name;
 
@@ -24,10 +42,23 @@ contract MetaDOSAssetOriginal is OERC1155Upgradeable, IERC1155ReceiverUpgradeabl
     // Mapping token id to total supply max
     mapping(uint256 => uint256) private _totalSupplyMax;
 
-    event UseSignature(address indexed from, uint256 id, uint256 nonce, uint256 value, bytes signature);
+    event UseSignature(
+        address indexed from,
+        uint256 indexed id,
+        uint256 indexed nonce,
+        uint256 value,
+        bytes signature
+    );
 
-    function initialize(string memory uri_, string memory name_, string memory symbol_, address endpoint_, address forwarder_) public initializer {
+    function initialize(
+        string memory uri_,
+        string memory name_,
+        string memory symbol_,
+        address endpoint_,
+        address forwarder_
+    ) public initializer {
         __OERC1155Upgradeable_init(uri_, endpoint_);
+        __EIP712_init("MetaDOSAsset", "0.0.1");
         _name = name_;
         _symbol = symbol_;
         _forwarder = forwarder_;
@@ -41,7 +72,13 @@ contract MetaDOSAssetOriginal is OERC1155Upgradeable, IERC1155ReceiverUpgradeabl
         return _forwarder;
     }
 
-    function _msgSender() internal view virtual override returns (address sender) {
+    function _msgSender()
+        internal
+        view
+        virtual
+        override
+        returns (address sender)
+    {
         if (getForwarder() == msg.sender) {
             assembly {
                 sender := shr(96, calldataload(sub(calldatasize(), 20)))
@@ -51,7 +88,13 @@ contract MetaDOSAssetOriginal is OERC1155Upgradeable, IERC1155ReceiverUpgradeabl
         }
     }
 
-    function _msgData() internal view virtual override returns (bytes calldata) {
+    function _msgData()
+        internal
+        view
+        virtual
+        override
+        returns (bytes calldata)
+    {
         if (getForwarder() == msg.sender) {
             return msg.data[:msg.data.length - 20];
         } else {
@@ -59,17 +102,24 @@ contract MetaDOSAssetOriginal is OERC1155Upgradeable, IERC1155ReceiverUpgradeabl
         }
     }
 
-    function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual override returns (bytes4) {
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes memory
+    ) public virtual override returns (bytes4) {
         return this.onERC1155Received.selector;
     }
 
-    function onERC1155BatchReceived(address, address, uint256[] memory, uint256[] memory, bytes memory) public virtual override returns (bytes4) {
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] memory,
+        uint256[] memory,
+        bytes memory
+    ) public virtual override returns (bytes4) {
         return this.onERC1155BatchReceived.selector;
-    }
-
-    modifier onlySigner() {
-        require(_signers[_msgSender()], "Caller is not the signer");
-        _;
     }
 
     function isSigner(address account) public view virtual returns (bool) {
@@ -88,7 +138,7 @@ contract MetaDOSAssetOriginal is OERC1155Upgradeable, IERC1155ReceiverUpgradeabl
         return _nonces[account];
     }
 
-    function setNonce(address account) public virtual onlySigner {
+    function setNonce(address account) public virtual onlyOwner {
         _nonces[account]++;
     }
 
@@ -100,11 +150,10 @@ contract MetaDOSAssetOriginal is OERC1155Upgradeable, IERC1155ReceiverUpgradeabl
         return _symbol;
     }
 
-    function setTokenURI(string memory uri) public virtual onlyOwner {
-        _setURI(uri);
-    }
-
-    function setTotalSupplyMax(uint256[] memory ids, uint256[] memory amounts) public virtual onlyOwner {
+    function setTotalSupplyMax(
+        uint256[] memory ids,
+        uint256[] memory amounts
+    ) public virtual onlyOwner {
         for (uint256 i = 0; i < ids.length; i++) {
             _totalSupplyMax[ids[i]] = amounts[i];
         }
@@ -114,19 +163,29 @@ contract MetaDOSAssetOriginal is OERC1155Upgradeable, IERC1155ReceiverUpgradeabl
         return _totalSupplyMax[id];
     }
 
-    function mint(address to, uint256 id, uint256 nonce, uint256 amount, bytes memory signature) public virtual {
-        bytes32 msgEncode = keccak256(abi.encodePacked(to, id, nonce, amount));
-        bytes32 msgHash = ECDSAUpgradeable.toEthSignedMessageHash(msgEncode);
-        address signer = ECDSAUpgradeable.recover(msgHash, signature);
+    function mint(
+        address to,
+        uint256 id,
+        uint256 nonce,
+        uint256 amount,
+        bytes memory signature
+    ) public virtual {
+        bytes32 msgHash = keccak256(
+            abi.encode(_MINTHASH, to, id, nonce, amount)
+        );
+        address signer = _hashTypedDataV4(msgHash).recover(signature);
 
-        require(isSigner(signer) && getNonce(to) == nonce, "Signature does not match request");
+        require(
+            isSigner(signer) && getNonce(to) == nonce,
+            "Signature does not match request"
+        );
 
         uint256 total = totalSupply(id) + amount;
         bool ok = (total <= totalSupplyMax(id));
         require(ok, "Mint amount exceeds total supply max");
 
-        _mint(to, id, amount, "");
         _nonces[to]++;
+        _mint(to, id, amount, "");
 
         emit UseSignature(to, id, nonce, amount, signature);
     }
