4e02c6d4d0cd8e5b28f19dbd5c9bbf6ed9adde28
dienhv <dienhouit05@gmail.com>
2023-10-17 17:35:40 +0700

    update audit medium

--- audits/MetaDOSAssetOriginal.sol
+++ audits/MetaDOSAssetOriginal.sol
@@ -3,26 +3,13 @@ pragma solidity ^0.8.0;
 
 import "./OERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155ReceiverUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
 
-contract MetaDOSAssetOriginal is
-    EIP712Upgradeable,
-    OERC1155Upgradeable,
-    IERC1155ReceiverUpgradeable
-{
+contract MetaDOSAssetOriginal is EIP712Upgradeable, OERC1155Upgradeable {
     using ECDSAUpgradeable for bytes32;
 
-    struct MintRequest {
-        address to;
-        uint256 id;
-        uint256 nonce;
-        uint256 amount;
-    }
-    bytes32 private constant _MINTHASH =
-        keccak256(
-            "MintRequest(address to,uint256 id,uint256 nonce,uint256 amount)"
-        );
+    // Mint request
+    bytes32 private constant _MINT_REQUEST_TYPEHASH = keccak256("MintRequest(address to,uint256 id,uint256 amount,uint256 nonce)");
 
     // Token name
     string private _name;
@@ -42,43 +29,25 @@ contract MetaDOSAssetOriginal is
     // Mapping token id to total supply max
     mapping(uint256 => uint256) private _totalSupplyMax;
 
-    event UseSignature(
-        address indexed from,
-        uint256 indexed id,
-        uint256 indexed nonce,
-        uint256 value,
-        bytes signature
-    );
+    event UseSignature(address indexed from, uint256 indexed id, uint256 value, bytes signature);
 
-    function initialize(
-        string memory uri_,
-        string memory name_,
-        string memory symbol_,
-        address endpoint_,
-        address forwarder_
-    ) public initializer {
+    function initialize(string calldata uri_, string calldata name_, string calldata symbol_, address endpoint_) public initializer {
         __OERC1155Upgradeable_init(uri_, endpoint_);
-        __EIP712_init("MetaDOSAsset", "0.0.1");
+        __EIP712_init(name_, "1");
         _name = name_;
         _symbol = symbol_;
-        _forwarder = forwarder_;
-    }
-
-    function setForwarder(address forwarder) public virtual onlyOwner {
-        _forwarder = forwarder;
     }
 
     function getForwarder() public view virtual returns (address) {
         return _forwarder;
     }
 
-    function _msgSender()
-        internal
-        view
-        virtual
-        override
-        returns (address sender)
-    {
+    function setForwarder(address forwarder) public virtual onlyOwner {
+        require(forwarder != address(0), "Invalid address");
+        _forwarder = forwarder;
+    }
+
+    function _msgSender() internal view virtual override returns (address sender) {
         if (getForwarder() == msg.sender) {
             assembly {
                 sender := shr(96, calldataload(sub(calldatasize(), 20)))
@@ -88,13 +57,7 @@ contract MetaDOSAssetOriginal is
         }
     }
 
-    function _msgData()
-        internal
-        view
-        virtual
-        override
-        returns (bytes calldata)
-    {
+    function _msgData() internal view virtual override returns (bytes calldata) {
         if (getForwarder() == msg.sender) {
             return msg.data[:msg.data.length - 20];
         } else {
@@ -102,24 +65,9 @@ contract MetaDOSAssetOriginal is
         }
     }
 
-    function onERC1155Received(
-        address,
-        address,
-        uint256,
-        uint256,
-        bytes memory
-    ) public virtual override returns (bytes4) {
-        return this.onERC1155Received.selector;
-    }
-
-    function onERC1155BatchReceived(
-        address,
-        address,
-        uint256[] memory,
-        uint256[] memory,
-        bytes memory
-    ) public virtual override returns (bytes4) {
-        return this.onERC1155BatchReceived.selector;
+    modifier onlySigner() {
+        require(_signers[_msgSender()], "Caller is not the signer");
+        _;
     }
 
     function isSigner(address account) public view virtual returns (bool) {
@@ -127,19 +75,26 @@ contract MetaDOSAssetOriginal is
     }
 
     function setSigner(address account) public virtual onlyOwner {
+        require(account != address(0), "Invalid address");
         _signers[account] = true;
     }
 
     function delSigner(address account) public virtual onlyOwner {
+        require(account != address(0), "Invalid address");
         delete _signers[account];
     }
 
-    function getNonce(address account) public view virtual returns (uint256) {
+    function nonces(address account) public view virtual returns (uint256) {
         return _nonces[account];
     }
 
-    function setNonce(address account) public virtual onlyOwner {
-        _nonces[account]++;
+    function useNonce(address account) public virtual onlyOwner {
+        require(account != address(0), "Invalid address");
+        _useNonce(account);
+    }
+
+    function _useNonce(address account) internal virtual returns (uint256) {
+        return _nonces[account]++;
     }
 
     function name() public view virtual returns (string memory) {
@@ -150,11 +105,10 @@ contract MetaDOSAssetOriginal is
         return _symbol;
     }
 
-    function setTotalSupplyMax(
-        uint256[] memory ids,
-        uint256[] memory amounts
-    ) public virtual onlyOwner {
+    function setTotalSupplyMax(uint256[] calldata ids, uint256[] calldata amounts) public virtual onlyOwner {
+        require(ids.length == amounts.length, "ids and amounts length mismatch");
         for (uint256 i = 0; i < ids.length; i++) {
+            require(amounts[i] > totalSupplyMax(ids[i]), "Invalid amount");
             _totalSupplyMax[ids[i]] = amounts[i];
         }
     }
@@ -163,30 +117,24 @@ contract MetaDOSAssetOriginal is
         return _totalSupplyMax[id];
     }
 
-    function mint(
-        address to,
-        uint256 id,
-        uint256 nonce,
-        uint256 amount,
-        bytes memory signature
-    ) public virtual {
-        bytes32 msgHash = keccak256(
-            abi.encode(_MINTHASH, to, id, nonce, amount)
-        );
-        address signer = _hashTypedDataV4(msgHash).recover(signature);
+    function burn(uint256 id, uint256 amount) public virtual {
+        _burn(_msgSender(), id, amount);
+    }
 
-        require(
-            isSigner(signer) && getNonce(to) == nonce,
-            "Signature does not match request"
-        );
+    function mint(address to, uint256 id, uint256 amount, bytes calldata signature) public virtual {
+        require(to != address(0), "Mint to the zero address");
+
+        bytes32 msgHash = keccak256(abi.encode(_MINT_REQUEST_TYPEHASH, to, id, amount, nonces(to)));
+        address signer = _hashTypedDataV4(msgHash).recover(signature);
+        require(isSigner(signer), "Signature does not match request");
 
         uint256 total = totalSupply(id) + amount;
         bool ok = (total <= totalSupplyMax(id));
         require(ok, "Mint amount exceeds total supply max");
 
-        _nonces[to]++;
+        _useNonce(to);
         _mint(to, id, amount, "");
 
-        emit UseSignature(to, id, nonce, amount, signature);
+        emit UseSignature(to, id, amount, signature);
     }
 }
--- audits/MetaDOSAssetWrapped.sol
+++ audits/MetaDOSAssetWrapped.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "./OERC1155Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 
 contract MetaDOSAssetWrapped is OERC1155Upgradeable {
     // Token name
@@ -11,7 +10,7 @@ contract MetaDOSAssetWrapped is OERC1155Upgradeable {
     // Token symbol
     string private _symbol;
 
-    function initialize(string memory uri_, string memory name_, string memory symbol_, address endpoint_) public initializer {
+    function initialize(string calldata uri_, string calldata name_, string calldata symbol_, address endpoint_) public initializer {
         __OERC1155Upgradeable_init(uri_, endpoint_);
         _name = name_;
         _symbol = symbol_;
@@ -23,9 +22,5 @@ contract MetaDOSAssetWrapped is OERC1155Upgradeable {
 
     function symbol() public view virtual returns (string memory) {
         return _symbol;
-    }
-
-    function setTokenURI(string memory uri) public virtual onlyOwner {
-        _setURI(uri);
     }
 }
