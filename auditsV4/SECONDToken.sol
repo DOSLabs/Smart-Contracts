0e90c29675374c9af2f5a94a26e415a50d2e2f60
dienhv <dienhouit05@gmail.com>
2024-02-06 13:53:57 +0700

    update contract token and packagejson

--- auditsV4/SECONDToken.sol
+++ auditsV4/SECONDToken.sol
@@ -1,14 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+import "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
 
-contract SECONDToken is ERC20 {
-  address public admin;
+contract SECONDToken is Ownable, ERC2771Context, OFT {
+  string private _name;
+  string private _symbol;
 
-  constructor(string memory name_, string memory symbol_, address admin_) ERC20(name_, symbol_) {
-    admin = admin_;
-    _mint(admin, 10_000_000_000 * 10 ** decimals());
+  constructor(string memory name_, string memory symbol_, address endpoint_, address forwarder_, address admin_, uint256 supply_) Ownable(msg.sender) ERC2771Context(forwarder_) OFT(name_, symbol_, endpoint_, msg.sender) {
+    _name = name_;
+    _symbol = symbol_;
+    if (admin_ != address(0) && supply_ > 0) _mint(admin_, supply_ * 10 ** decimals());
+  }
+
+  function _msgSender() internal view virtual override(Context, ERC2771Context) returns (address) {
+    return super._msgSender();
+  }
+
+  function _msgData() internal view virtual override(Context, ERC2771Context) returns (bytes calldata) {
+    return super._msgData();
+  }
+
+  function _contextSuffixLength() internal view virtual override(Context, ERC2771Context) returns (uint256) {
+    return super._contextSuffixLength();
+  }
+
+  function name() public view virtual override returns (string memory) {
+    return _name;
+  }
+
+  function symbol() public view virtual override returns (string memory) {
+    return _symbol;
   }
 
   function burn(uint256 value) public virtual {
